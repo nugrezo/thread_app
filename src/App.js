3e7601274703e5f7739b31c7ec7e501bfffeb8ca
Ergun Oz <ergun.oz82@gmail.com>
2024-01-24 00:12:57 -0500

    trying

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import "./App.css";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {" "}
-      <div>
-        {/*The following code snippet utilizes React Router's 
+      {/*The following code snippet utilizes React Router's 
       BrowserRouter to manage navigation within the application.
       Each defined Route corresponds to a specific URL path, 
       rendering the associated component when the path is matched.*/}
 
-        <BrowserRouter>
-          <div>Hi there</div>
-          <Routes>
-            <div>Hi there</div>
-          </Routes>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <h1>Hello React</h1>
+      </BrowserRouter>
     </div>
   );
 }
