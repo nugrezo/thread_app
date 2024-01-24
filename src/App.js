b3ad719f69a7be0c679e04edf5c6fc3afcfae825
Ergun Oz <ergun.oz82@gmail.com>
2024-01-23 23:48:40 -0500

    route changed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
         <BrowserRouter>
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route path="/thread_app/login" element={<Login />} />
           </Routes>
         </BrowserRouter>
       </div>
